436c58453c7659da62bfa47e85ff4008c31aac13
DimaSimakov <dimarodeo@gmail.com>
2023-08-05 21:32:36 +0700

    add print students to html

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
-from main.views import index
+from main.views import index_2
 
 urlpatterns = [
-    path('', index),
+    path('', index_2),
 ]
--- main/views.py
+++ main/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
 
+from main.models import Student
 
-def index(request):
-    if request.method == 'POST':
-        # в переменной request хранится информация о методе, который отправлял пользователь
-        name = request.POST.get('name')
-        # а также передается информация, которую заполнил пользователь
-        email = request.POST.get('email')
-        message = request.POST.get('message')
-        print(f'Имя - {name}, почта - {email}, сообщение - {message}')
 
-    return render(request, 'main/index.html')
+# def index(request):
+#     if request.method == 'POST':
+#         # в переменной request хранится информация о методе, который отправлял пользователь
+#         name = request.POST.get('name')
+#         # а также передается информация, которую заполнил пользователь
+#         email = request.POST.get('email')
+#         message = request.POST.get('message')
+#         print(f'Имя - {name}, почта - {email}, сообщение - {message}')
+#
+#     return render(request, 'main/index.html')
+
+
+def index_2(request):
+    stud_list = Student.objects.all()
+    context = {
+        'object_list': stud_list
+    }
+    for student in stud_list:
+        print(student)
+
+    return render(request, 'main/test_html.html', context)
