3e29bf8231dc3cb66090c08f61cde57d51310e00
DimaSimakov <dimarodeo@gmail.com>
2023-08-06 13:37:39 +0700

    add contact page contact.html

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 
-from main.views import index_2
+from main.views import index_2, contact
 
 urlpatterns = [
     path('', index_2),
+    path('contact/', contact),
 ]
--- main/views.py
+++ main/views.py
@@ -3,16 +3,16 @@ from django.shortcuts import render
 from main.models import Student
 
 
-# def index(request):
-#     if request.method == 'POST':
-#         # в переменной request хранится информация о методе, который отправлял пользователь
-#         name = request.POST.get('name')
-#         # а также передается информация, которую заполнил пользователь
-#         email = request.POST.get('email')
-#         message = request.POST.get('message')
-#         print(f'Имя - {name}, почта - {email}, сообщение - {message}')
-#
-#     return render(request, 'main/index.html')
+def contact(request):
+    if request.method == 'POST':
+        # в переменной request хранится информация о методе, который отправлял пользователь
+        name = request.POST.get('name')
+        # а также передается информация, которую заполнил пользователь
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'Имя - {name}, почта - {email}, сообщение - {message}')
+
+    return render(request, 'main/contact.html')
 
 
 def index_2(request):
