36f98f9e3eeafaf4333e637d5b9acd4e8fcc96d6
DimaSimakov <dimarodeo@gmail.com>
2023-08-09 15:47:48 +0700

    changed controllers from FBV to CBV

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
 
 from main.apps import MainConfig
-from main.views import index_2, contact, view_student
+from main.views import contact, StudentListView, StudentDetailView
 
 app_name = MainConfig.name
 
 urlpatterns = [
-    path('', index_2, name='test_html'),
+    # path('', index_2, name='test_html'),
+    path('', StudentListView.as_view(), name='test_html'),
     path('contact/', contact, name='contact'),
-    path('<int:pk>/view_student', view_student, name='view_student'),
+    # path('view_student/<int:pk>/', view_student, name='view_student'),
+    path('view_student/<int:pk>/', StudentDetailView.as_view(), name='view_student'),
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from main.models import Student
 
 
+class StudentListView(ListView):
+    """
+    Выводит информаццию о всех студентах на главную страницу вместо функции index_2
+    """
+
+    model = Student
+    template_name = 'main/test_html.html'
+
+
+class StudentDetailView(DetailView):
+    """
+    Выводит информаццию об одном, выбранном на главной странице, студенте вместо функции view_student
+    """
+    model = Student
+    template_name = 'main/view_student.html'
+
+
 def contact(request):
+    """
+    Выводит страницу с контактами и получает обратную связь
+    """
+
     if request.method == 'POST':
         # в переменной request хранится информация о методе, который отправлял пользователь
         name = request.POST.get('name')
@@ -19,24 +41,30 @@ def contact(request):
     return render(request, 'main/contact.html', context)
 
 
-def index_2(request):
-    stud_list = Student.objects.all()
-    context = {
-        'object_list': stud_list,
-        'title': 'Главная'
-    }
-    for student in stud_list:
-        print(student)
+# def index_2(request):
+#     """
+#     Выводит информаццию о всех студентах на главную страницу
+#     """
+#     stud_list = Student.objects.all()
+#     context = {
+#         'object_list': stud_list,
+#         'title': 'Главная'
+#     }
+#     for student in stud_list:
+#         print(student)
+#
+#     return render(request, 'main/test_html.html', context)
 
-    return render(request, 'main/test_html.html', context)
 
-
-def view_student(request, pk):
-
-    student_info = Student.objects.get(pk=pk)  # получаем данные студента по его id
-    context = {
-        'student_info': student_info,
-        'title': 'Главная'
-    }
-
-    return render(request, 'main/view_student.html', context)
+# def view_student(request, pk):
+#     """
+#     Выводит информаццию об одном, выбранном на главной странице, студенте
+#     """
+#
+#     student_info = Student.objects.get(pk=pk)  # получаем данные студента по его id
+#     context = {
+#         'student_info': student_info,
+#         'title': 'Данные студента'
+#     }
+#
+#     return render(request, 'main/view_student.html', context)
