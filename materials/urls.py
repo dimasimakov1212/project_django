df9e39b56b53875bc83b9d3bc0594c1fffdb4b79
dimas <dimarodeo@gmail.com>
2023-08-17 03:42:34 +0700

    add new controller and material_confirm_delete.html

--- materials/urls.py
+++ materials/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 
 from materials.apps import MaterialsConfig
-from materials.views import MaterialCreateView, MaterialListView, MaterialDetailView
+from materials.views import MaterialCreateView, MaterialListView, MaterialDetailView, MaterialUpdateView, \
+    MaterialDeleteView
 
 app_name = MaterialsConfig.name
 
@@ -9,6 +10,6 @@ urlpatterns = [
     path('create/', MaterialCreateView.as_view(), name='create'),
     path('', MaterialListView.as_view(), name='list'),
     path('view/<int:pk>/', MaterialDetailView.as_view(), name='view'),
-    # path('edit/<int:pk>/', ..., name='edit'),
-    # path('delete/<int:pk>/', ..., name='delete'),
+    path('edit/<int:pk>/', MaterialUpdateView.as_view(), name='edit'),
+    path('delete/<int:pk>/', MaterialDeleteView.as_view(), name='delete'),
 ]
--- materials/views.py
+++ materials/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView
 
 from materials.models import Material
 
@@ -16,14 +16,31 @@ class MaterialCreateView(CreateView):
 
 class MaterialListView(ListView):
     """
-    Выводит информацию о материале
+    Выводит информацию о материалах
     """
     model = Material
 
 
 class MaterialDetailView(DetailView):
     """
-    Выводит информаццию об одном, выбранном на главной странице, студенте вместо функции view_student
+    Выводит информаццию об одном, выбранном на главной странице, материале
     """
     model = Material
     template_name = 'materials/view_material.html'
+
+
+class MaterialUpdateView(UpdateView):
+    """
+    Выводит форму редактирования материала
+    """
+    model = Material
+    fields = ('material_title', 'material_body',)
+    success_url = reverse_lazy('materials:list')
+
+
+class MaterialDeleteView(DeleteView):
+    """
+    Выводит форму удаления материала
+    """
+    model = Material
+    success_url = reverse_lazy('materials:list')
