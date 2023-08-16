0b362c1ee13e206f32212fe7a6cea2060c07aa32
dimas <dimarodeo@gmail.com>
2023-08-16 20:33:26 +0700

    add new controller and material_list.html and view_material.html

--- materials/urls.py
+++ materials/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path, include
 
 from materials.apps import MaterialsConfig
-from materials.views import MaterialCreateView
+from materials.views import MaterialCreateView, MaterialListView, MaterialDetailView
 
 app_name = MaterialsConfig.name
 
 urlpatterns = [
     path('create/', MaterialCreateView.as_view(), name='create'),
-    # path('', ..., name='list'),
-    # path('view/<int:pk>/', ..., name='view'),
+    path('', MaterialListView.as_view(), name='list'),
+    path('view/<int:pk>/', MaterialDetailView.as_view(), name='view'),
     # path('edit/<int:pk>/', ..., name='edit'),
     # path('delete/<int:pk>/', ..., name='delete'),
 ]
--- materials/views.py
+++ materials/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, ListView
 
 from materials.models import Material
 
@@ -11,7 +11,14 @@ class MaterialCreateView(CreateView):
     """
     model = Material
     fields = ('material_title', 'material_body',)
-    success_url = reverse_lazy('main:test_html')
+    success_url = reverse_lazy('materials:list')
+
+
+class MaterialListView(ListView):
+    """
+    Выводит информацию о материале
+    """
+    model = Material
 
 
 class MaterialDetailView(DetailView):
@@ -19,4 +26,4 @@ class MaterialDetailView(DetailView):
     Выводит информаццию об одном, выбранном на главной странице, студенте вместо функции view_student
     """
     model = Material
-    template_name = 'main/view_student.html'
+    template_name = 'materials/view_material.html'
