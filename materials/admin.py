1b5cd5deb90269335391dd7117946a1484d46239
dimas <dimarodeo@gmail.com>
2023-08-19 16:30:28 +0700

    add admin registration for app materials

--- materials/admin.py
+++ materials/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from materials.models import Material
+
+
+# admin.site.register(Student)
+
+
+@admin.register(Material)
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ('material_title', 'views_count', 'is_published',)
+    list_filter = ('material_title',)
+    search_fields = ('material_title', 'last_name',)
