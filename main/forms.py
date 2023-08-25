13fe036f6d0ec3538f0773ee0a9ddcd2f443a10b
DimaSimakov <dimarodeo@gmail.com>
2023-08-25 18:15:01 +0700

    add new field in models.py for app main

--- main/forms.py
+++ main/forms.py
@@ -16,8 +16,25 @@ class StudentForm(forms.ModelForm):
 
         # используется какой-то один параметр
         # fields = '__all__'  # выводит в форму все поля
-        fields = ('first_name', 'last_name',)  # выводит в форму указанные поля
+        fields = ('first_name', 'last_name', 'email')  # выводит в форму указанные поля
         # exclude = ('is_active',)  # выводит в форму все поля, кроме указанного
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+    def clean_email(self):
+        """
+        Проверяет корректность ввода email
+        """
+        cleaned_data = self.cleaned_data['email']
+
+        if 'sky.pro' not in cleaned_data:
+            raise forms.ValidationError('Почта должна относиться к учебному заведению')
+
+        return cleaned_data
 
 
 class SubjectForm(forms.ModelForm):
@@ -27,3 +44,9 @@ class SubjectForm(forms.ModelForm):
     class Meta:
         model = Subject
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
