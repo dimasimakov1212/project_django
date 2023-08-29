aebcf52d3873a3661c52646bca84c9b5558ee623
DimaSimakov <dimarodeo@gmail.com>
2023-08-29 22:46:51 +0700

    add user profile

--- users/views.py
+++ users/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, UpdateView
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
-# Create your views here.
+from users.forms import UserRegisterForm, UserProfileForm
+from users.models import User
+
+
+class RegisterView(CreateView):
+    """
+
+    """
+    model = User
+    form_class = UserRegisterForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('users:login')
+
+
+class ProfileView(UpdateView):
+    """
+
+    """
+    model = User
+    form_class = UserProfileForm
+    # template_name = 'users/register.html'
+    success_url = reverse_lazy('users:profile')
+
+    def get_object(self, queryset=None):
+        """
+        Позволяет делать необязательным передачу pk объекта
+        """
+        return self.request.user
+
+    # def __init__(self, *args, **kwargs):
+    #     """
+    #     Позволяет не выводить в профиле поле пароля
+    #     """
+    #     super().__init__(*args, **kwargs)
+    #     self.fields['password'].widget = forms.HiddenInput()
